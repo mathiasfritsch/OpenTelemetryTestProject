c24c064f2df3444c2a319059d1ee8cd54ece7073
Mathias Fritsch <mathias.fritsch.extern@rwe.com>
2024-01-01 20:10:49 +0100

    initial steup

---
+++ ConsumerApi/Controllers/MessageController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumerApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessageController : ControllerBase
+    {
+
+        private readonly ILogger<MessageController> _logger;
+
+        public MessageController(ILogger<MessageController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetMessage")]
+        public string Get()
+        {
+            return "ok";
+        }
+    }
+}
